5b4e388081a12533b84755a776bc2e57ccfda8ee
otabbaa <tabbaa.2@gmail.com>
2017-12-30 13:38:38 -0500

    Added Euler Method Deterministi Simulation Of Rxs

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import random 
+import random
 from math import log1p
 
 rx = [
-    # gene          # dependencies    # signals     # modifications                       # rate 
+    # gene          # dependencies    # signals     # modifications                       # rate
     ("gene1",       ["g1"],           ["s1"],       [("g1", -1), ("ga1", 1)],             0.1),
     ("gene1",       ["ga1"],          [],           [("g1", 1), ("ga1", -1)],             0.05),
     ("gene1",       ["ga1"],          [],           [("m1", 1)],                          0.1),
@@ -23,15 +23,15 @@ signals_1 = {"s1" : 0.1}
 signals_2 = {"s1" : 1.0}
 
 initial_state = {
-    "g1"  : 1, 
-    "ga1" : 0, 
-    "m1"  : 0, 
-    "p1"  : 0, 
+    "g1"  : 1,
+    "ga1" : 0,
+    "m1"  : 0,
+    "p1"  : 0,
 
-    "g2"  : 1, 
-    "ga2" : 0, 
-    "m2"  : 0, 
-    "p2"  : 0, 
+    "g2"  : 1,
+    "ga2" : 0,
+    "m2"  : 0,
+    "p2"  : 0,
 }
 
 
@@ -41,13 +41,13 @@ def propensity(rx, state, signals):
         if len(depends) > 1 and depends[0] == depends[1]:
             a.append(state[depends[0]] * (state[depends[1]] - 1) * rate/2)
         else:
-            p = 1
+            dep = 1
             for m in depends:
-                p = p * state[m] 
+                dep = dep * state[m]
 
             for s in sig:
-                p = p * signals[s]
-            a.append(p * rate)
+                dep = dep * signals[s]
+            a.append(dep * rate)
     return a, sum(a)
 
 def update_time(t, b):
@@ -58,30 +58,30 @@ def reaction(rx, state, signals, t):
     a, b = propensity(rx, state, signals)
     a_sorted = sorted(list(enumerate(a)), key=lambda x: x[1], reverse=True)
     rb = random.uniform(0, 1) * b
-	
-    m, MU = 0, 0 	
+
+    m, MU = 0, 0
     for i, p in a_sorted:
-        m = m + p 
+        m = m + p
         if m > rb:
             MU = i
             break
-    
+
     _, _, _, mods, _ = rx[MU]
     for mol, count in mods:
         state[mol] += count
-    
+
     t = update_time(t, b)
     return t
 
 
-def simulate(rx, initial_state, signals, t_stop, t_check, n):
+def stoch_sim(rx, initial_state, signals, t_stop, t_check, n):
     state = initial_state.copy()
     results = []
 
     if t_stop == None:
-        n_ss = 2 
+        n_ss = 2
         t_stop = float(n_ss)/min(rate for _, _, _, _, rate in rx)
-    
+
     for i in range(n):
         t = 0
         run_results = []
@@ -93,15 +93,60 @@ def simulate(rx, initial_state, signals, t_stop, t_check, n):
         results.append(run_results)
     return results
 
+def ode_sim(rx, initial_state, signals, t_stop, t_check):
+    state = initial_state.copy()
+    state_temp = state.copy()
+    results = []
+
+    if t_stop == None:
+        n_ss = 2
+        t_stop = float(n_ss)/min(rate for _, _, _, _, rate in rx)
+
+    t = 0
+    if 0.001/max(rate for _, _, _, _, rate in rx) <= 0.1:
+        dt = 0.001/max(rate for _, _, _, _, rate in rx)
+    else:
+        dt = 0.1
+
+    run_results = []
+    while t < t_stop:
+        for gene, depends, sig, mods, rate in rx:
+            dep = 1
+            for mol in depends:
+                dep = dep * state[mol]
+            for s in sig:
+                dep = dep * signals[s]
+
+            for mol, sign in mods:
+                state_temp[mol] += rate * dep * sign * dt
+
+        state = state_temp.copy()
+
+        t = t + dt
+        if t > t_check:
+            run_results.append((t, state.copy()))
+
+        results.append(run_results)
+    return results
+
 def main():
     t_check = 0.1
-    t_stop = None 
-    n = 10
-    
-    results_1, results_2 = simulate(rx, initial_state, signals_1, t_stop, t_check, n), \
-                           simulate(rx, initial_state, signals_2, t_stop, t_check, n)
-    
+    t_stop = None
+    n = 1000
+
+    results_1, results_2 = stoch_sim(rx, initial_state, signals_1, t_stop, t_check, n), \
+                           stoch_sim(rx, initial_state, signals_2, t_stop, t_check, n)
+
     for results in (results_1, results_2):
+        # print(results)
+        p2_dist = [run_results[-1][1]['p2'] for run_results in results]
+        p2_mean = sum(p2_dist)/float(len(p2_dist))
+        print(p2_mean)
+
+    results_3, results_4 = ode_sim(rx, initial_state, signals_1, t_stop, t_check), \
+                           ode_sim(rx, initial_state, signals_2, t_stop, t_check)
+
+    for results in (results_3, results_4):
         # print(results)
         p2_dist = [run_results[-1][1]['p2'] for run_results in results]
         p2_mean = sum(p2_dist)/float(len(p2_dist))
